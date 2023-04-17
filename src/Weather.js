5879b7d6265d392415b3d0b6b4eb11cd6b13066a
Goli2023 <122437095+Goli2023@users.noreply.github.com>
2023-04-17 12:32:53 +0330

    Importing sandbox to VS code

---
+++ src/Weather.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function showWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humiduty: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "a2dda52dce059eb8a14e95aaa0db6ab7";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(showWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a city..."
+        onChange={updateCity}
+      />
+      <button type="submit"> Search </button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature:{Math.round(weather.temperature)}°C</li>
+          <li>Humidity:{weather.humidity}%</li>
+          <li>Description: {weather.description}</li>
+          <li>Wind: {Math.round(weather.wind)}km/h</li>
+        </ul>
+        <p>
+          <img src={weather.icon} alt={weather.description} />
+        </p>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import Weather from "./Weather";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <h1>Weather App</h1>
+    <Weather />
+  </StrictMode>
+);
